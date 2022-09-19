6b87af4f7c932e4308cac96e5994afb6e338fa65
Moritz Ulrich <moritz@tarn-vedra.de>
2022-09-19 18:02:03 +0200

    Use thiserror instead of failure

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,17 @@
-#[macro_use]
-extern crate failure;
-
-use serde_json;
-
 mod recipe;
 pub use recipe::*;
 
 mod recipe_set;
 pub use recipe_set::*;
 
-#[derive(Debug, Fail)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
-    #[fail(display = "IO Error")]
-    Io(std::io::Error),
-    #[fail(display = "Serde Error")]
-    Serde(serde_json::Error),
-    #[fail(display = "ZIP Error")]
-    Zip(zip::result::ZipError),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Error {
-        Error::Io(e)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(e: serde_json::Error) -> Error {
-        Error::Serde(e)
-    }
-}
-
-impl From<zip::result::ZipError> for Error {
-    fn from(e: zip::result::ZipError) -> Error {
-        Error::Zip(e)
-    }
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Serde Error: {0}")]
+    Serde(#[from] serde_json::Error),
+    #[error("ZIP Error: {0}")]
+    Zip(#[from] zip::result::ZipError),
 }
